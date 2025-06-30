9b76cd3739d2b4c155a7946180de333fb968a6b5
Syed Ali Shoaib <alishoaib007@yahoo.com>
2025-06-30 11:33:26 +0500

    Updated icons

--- frontend/src/components/HeroLanding.jsx
+++ frontend/src/components/HeroLanding.jsx
@@ -213,6 +213,12 @@ export default function HeroLanding() {
                     <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/>
                   </svg>
                 </a>
+                <a href='mailto:syedalishoaib.14@gmail.com' className="mt-1 text-gray-600 hover:text-blue-600 dark:text-gray-400 dark:hover:text-red-400 transition-colors group">
+                  <span className="sr-only">Email</span>
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                  </svg>
+                </a>
                 
               </div>
             </div>
